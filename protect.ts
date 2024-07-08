0a3ff200348a5689b1f92580346ba535ead17a7d
Tim Rowe <tim@tjsr.id.au>
2024-07-08 20:10:35 +1000

    Fix to update a branch with no settings.

--- protect.ts
+++ protect.ts
@@ -1,9 +1,9 @@
 import * as commander from "commander";
 
+import { GetResponseDataTypeFromEndpointMethod, RequestParameters } from "@octokit/types";
 import { RepoBranchInfo, RepoInfo } from "./types.js";
 
 import { Octokit } from "@octokit/rest";
-import { RequestParameters } from "@octokit/types";
 import assert from 'assert';
 import { checkSync } from "git-state";
 import gitRemoteOriginUrl from 'git-remote-origin-url';
@@ -129,12 +129,18 @@ export const createBranchProtectionSettingsPayload = (
   assert(branch !== undefined);
   assert(repoOwner !== undefined);
   assert(repoName !== undefined);
+  assert(enforceAdmins !== undefined);
 
   const pullRequestReviews: any = {
     dismiss_stale_reviews: true,
     require_code_owner_reviews: false,
   };
+  if (reviewers !== undefined) {
+    pullRequestReviews.required_approving_review_count = reviewers
+  }
+
   const branchProtectionSettings: any = {
+    enforce_admins: enforceAdmins,
     owner: repoOwner,
     repo: repoName,
     branch: branch,
@@ -143,12 +149,6 @@ export const createBranchProtectionSettingsPayload = (
     restrictions: null
   };
 
-  if (reviewers !== undefined) {
-    pullRequestReviews.required_approving_review_count = reviewers
-  }
-  if (enforceAdmins !== undefined) {
-    branchProtectionSettings.enforce_admins = enforceAdmins;
-  }
   return branchProtectionSettings;
 };
 
@@ -156,9 +156,22 @@ export const retrieveBranchProtectionSettings = async (octo: Octokit, params?: (
   owner: string;
   repo: string;
   branch: string;
-}) | undefined) => {
+}) | undefined): Promise<GetResponseDataTypeFromEndpointMethod<typeof octo.rest.repos.getBranchProtection>> => {
   return octo.rest.repos.getBranchProtection(params);
-}
+};
+
+const parameterOrExistingOrDefault = <DataType>(
+  param: DataType|undefined, existing: DataType|undefined, defaultValue: DataType|undefined
+): DataType|undefined => {
+  if (param !== undefined) {
+    return param;
+  }
+  if (existing !== undefined) {
+    return existing;
+  }
+  return defaultValue;
+};
+
 
 export const protectCommand = ():commander.Command => {
   const protect = new commander.Command("protect");
@@ -169,8 +182,9 @@ export const protectCommand = ():commander.Command => {
     .option("-p, --path <path>", "Location of repository if reading details from git repo on disk")
     .option("-b, --branch <branch>", "Repository branch to modify")
     .option('-n --reviewers <number>', 'Number of reviewers required for PR approval')
-    .option('-e, --enforce-admins', 'Also enforce branch protection rules applying to admins')
+    .option('-e, --enforce-admins', 'Also enforce branch protection rules applying to admins (default: true for new configs)')
     .option("-t, --token <token>", "GitHub token")
+    .option('-q, --query', 'Query the current branch protection settings')
     .action(async (options, command: commander.Command) => {
       const repoBranchInfo: RepoBranchInfo = await getRepoBranchInfo(options.owner, options.repo, options.branch, options.path);
       const token = options.token || process.env['GITHUB_TOKEN'];
@@ -201,33 +215,56 @@ export const protectCommand = ():commander.Command => {
       }
 
       const octo: Octokit = new Octokit({ auth: token });
-      console.log(repoBranchString(repoBranchInfo));
+      const repoString = repoBranchString(repoBranchInfo);
+      console.log(repoString);
       try {
         if (repoBranchInfo.repo === 'test') {
           console.log('Skipping in test mode.');
           return;
         }
 
-        const currentSettings = await retrieveBranchProtectionSettings(octo, repoBranchInfo);
-        console.log(currentSettings);
+        let currentSettings:GetResponseDataTypeFromEndpointMethod<typeof octo.rest.repos.getBranchProtection>|undefined = undefined;
 
-        const branchProtectionSettings = createBranchProtectionSettingsPayload(
+        try {
+          currentSettings = await retrieveBranchProtectionSettings(octo, repoBranchInfo);
+          // console.log(currentSettings);
+        } catch (err: any) {
+          if (err.status === 404) {
+            console.log(`Branch for repo ${repoString} is not currently protected.`);
+          } else {
+            console.debug('Error in retrieveBranchProtectionSettings', err);
+            command.error(`Failed to retrieve branch protection settings: ${err.message}`);
+            return;
+          }
+        }
+
+        const reviewersValue = parameterOrExistingOrDefault(
+          reviewers,
+          currentSettings?.enabled ? currentSettings?.required_pull_request_reviews?.required_approving_review_count : undefined,
+          0
+        );
+        const enforceAdmins = parameterOrExistingOrDefault(
+          options?.enforceAdmins, currentSettings?.enabled && currentSettings?.enforce_admins?.enabled,
+          true
+        );
+
+        const branchProtectionSettings = createBranchProtectionSettingsPayload( 
           repoBranchInfo.branch,
           repoBranchInfo.owner,
           repoBranchInfo.repo,
-          reviewers,
-          options?.enforceAdmins);
+          reviewersValue,
+          enforceAdmins);
         await octo.rest.repos.updateBranchProtection(branchProtectionSettings);
 
         const pullRequestReviewProtection = createReviewProtection(
           repoBranchInfo.branch,
           repoBranchInfo.owner,
           repoBranchInfo.repo,
-          reviewers
+          reviewersValue
         );
         await octo.rest.repos.updatePullRequestReviewProtection(pullRequestReviewProtection);
 
-        console.log('Updated branch protection.');
+        console.log(`Updated branch protection on ${repoString}`);
       } catch (err: any) {
         if (err.status === 401 && err.message) {
           console.error(err.message);
