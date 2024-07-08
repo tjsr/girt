cb36d5aa169e3b1d6e8382c73d6a52dbf54b93cc
Tim Rowe <tim@tjsr.id.au>
2024-07-08 16:46:15 +1000

    Fix version number output.

---
+++ protect.system.test.ts
@@ -0,0 +1,36 @@
+import { BranchProtectionContext } from "./testTypes.js";
+import { Octokit } from "@octokit/rest";
+import { retrieveBranchProtectionSettings } from "./protect.js";
+
+const hasGithubToken = process.env['GITHUB_TOKEN'] !== undefined;
+
+type OctokitBranchProtectionContext = BranchProtectionContext & {
+  githubToken: string;
+  octokit: Octokit;
+};
+
+const octoContext = (context: OctokitBranchProtectionContext): void => {
+  context.branch = 'main';
+  context.repoOwner = 'owner';
+  context.repoName = 'repo';
+  context.reviewers = 3;
+  context.enforceAdmins = false;
+  context.githubToken = process.env['GITHUB_TOKEN'] as string;
+};
+
+describe('retrieveBranchProtectionSettings', () => {
+  beforeEach((context: OctokitBranchProtectionContext) => {
+    octoContext(context);
+    context.octokit = new Octokit({
+      auth: context.githubToken
+    });
+  });
+
+  test.runIf(hasGithubToken)('Should return the branch protection settings for the specified branch', async (context: OctokitBranchProtectionContext) => {
+    await expect(retrieveBranchProtectionSettings(context.octokit, {
+      owner: context.repoOwner,
+      repo: context.repoName,
+      branch: context.branch
+    })).resolves.toHaveProperty('data');
+  });
+});
