8273f7be58e1b4c906abb8d3f6468261597c190e
Tim Rowe <tim@tjsr.id.au>
2024-07-15 12:37:32 +1000

    Fix branch name character replacement.

--- utils/branchNameToVersion.test.ts
+++ utils/branchNameToVersion.test.ts
@@ -1,7 +1,28 @@
 import { branchNameToVersion } from "./branchNameToVersion";
 
 describe('branchNameToVersion', () => {
-  test('Should replace all non-alphanumeric characters except space with a dash', () => {
+  test('Should replace disallowed characters except space with a dash', () => {
     expect(branchNameToVersion('feature/branch')).toBe('feature-branch');
+    expect(branchNameToVersion('fix/bug-name')).toBe('fix-bug-name');
+    expect(branchNameToVersion('fix/another_bug')).toBe('fix-another_bug');
+    expect(branchNameToVersion('something.else/version1.23')).toBe('something.else-version1.23');
+  });
+
+  test('Should replace ')
+
+  test('Should accept branch names containing numbers', () => {
+    expect(branchNameToVersion('dev/task123')).toBe('dev-task123');
+  });
+
+  test('Should accept branch names containing numbers', () => {
+    expect(branchNameToVersion('dev/task123')).toBe('dev-task123');
+  });
+
+  test('Should reject branch names beginning with a /', () => {
+    expect(() => branchNameToVersion('/dev/task')).toThrow();
+  });
+
+  test('Should reject branch names containing invalid characters', () => {
+    expect(() => branchNameToVersion('dev/illegal$name')).toThrow();
   });
 });
--- utils/branchNameToVersion.ts
+++ utils/branchNameToVersion.ts
@@ -1,5 +1,11 @@
 export const branchNameToVersion = (branchName: string): string => {
-  branchName = branchName.replace(/[^a-zA-Z0-9\w]/g, '-');
-  branchName = branchName.replace(/\w/g, '_');
+  if (branchName?.startsWith('/')) {
+    throw new Error('Branch name cannot start with a /');
+  }
+  if (branchName?.includes('$')) {
+    throw new Error('Branch name cannot contain $');
+  }
+  branchName = branchName.replace(/[^a-zA-Z0-9\w\.]/g, '-');
+  branchName = branchName.replace(/[^\w\-\.]/g, '_');
   return branchName;
 };
