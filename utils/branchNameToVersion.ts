5418068f9c09ac5339035774acad50f332a4be08
Tim Rowe <tim@tjsr.id.au>
2024-07-15 14:26:01 +1000

    Fix validation of branch names.

--- utils/branchNameToVersion.ts
+++ utils/branchNameToVersion.ts
@@ -1,10 +1,13 @@
+const validBranch = (branchName: string): boolean => {
+  const regex = /^(?!\/)[a-zA-Z0-9_\-\/.]+$/;
+  return regex.test(branchName);
+};
+
 export const branchNameToVersion = (branchName: string): string => {
-  if (branchName?.startsWith('/')) {
-    throw new Error('Branch name cannot start with a /');
+  if (!validBranch(branchName)) {
+    throw new Error(`Branch name ${branchName} is invalid`);
   }
-  if (branchName?.includes('$')) {
-    throw new Error('Branch name cannot contain $');
-  }
+
   branchName = branchName.replace(/[^a-zA-Z0-9\w\.]/g, '-');
   branchName = branchName.replace(/[^\w\-\.]/g, '_');
   return branchName;
